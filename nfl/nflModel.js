f3c04bb8acf0c4292dff095c92af073942e713b9
Anthony Hill <hillan1152@gmail.com>
2019-12-19 18:16:39 -0500

    made first test

--- nfl/nflModel.js
+++ nfl/nflModel.js
@@ -1,4 +1,4 @@
-const db = require('../data/db.Config.js');
+const db = require('../data/dbConfig.js');
 
 module.exports ={
     add, 
--- nfl/nflModel.spec.js
+++ nfl/nflModel.spec.js
@@ -1,6 +1,16 @@
-const Hobbits = require('./nflModel.js');
+const NFL = require('./nflModel.js');
 const db = require('../data/dbConfig');
 
 describe("nfl model", function(){
+    beforeEach(async () => {
+        await db('teams').truncate();
+    });
+    describe("add", function() {
+        it("This should add a team to the NFL DB", async function() {
+            await NFL.add({ team_name: "Bucs" })
 
+            const squad = await db("teams");
+            expect(squad).toHaveLength(1);
+        })
+    })
 })
